8a29c6650537da2a8bab0aaa852699d4df811023
doalfull <doalfull@gmail.com>
2022-04-08 19:31:47 -0500

    Manejando los nombres de variables y constantes para java

---
+++ src/NamingJava.java
@@ -0,0 +1,18 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone= 2222222;
+        int celPhone= 52222222;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName= "Spain";
+        String _backgroundColor= "Green";
+        String currency$ ="MXN";
+        String background_color ="BLUE";
+
+        int POSITION = -5;
+        int MAX_WITH = 9999;
+        int MIN_WITH = 1;
+    }
+}
