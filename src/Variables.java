32863b956e136cf2f33f5944132348c87a746a7a
doalfull <doalfull@gmail.com>
2022-04-08 19:03:29 -0500

    Creando variables en java

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+//declarando una variable
+        int speed;
+        // Asignamos un valora la variable
+        speed=10;
+        System.out.println(speed);
+
+        int salary=1000;
+        String employeeName="Aldo Almaraz";
+        System.out.println(employeeName);
+    }
+}
